b4e92f0f5e4b60b11190b0b0565e8d74002731d6
Vlada B <vlada.bessikalo@gmail.com>
2025-01-28 18:37:50 +0100

    finish assignments

--- week1/project/ecommerce/src/App.jsx
+++ week1/project/ecommerce/src/App.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/prop-types */
 import './App.css';
+import { useState } from 'react';
 import allCategories from './fake-data/all-categories.js';
 import products from './fake-data/all-products.js';
 import Header from './components/Header.jsx';
@@ -7,12 +7,34 @@ import Categories from './components/Categories.jsx';
 import ProductList from './components/ProductList.jsx';
 
 export default function App() {
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    const handleCategorySelect = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const normalizeCategory = (category) => {
+        return category.replace('FAKE:', '').trim();
+    };
+
+    const filteredProducts = selectedCategory
+        ? products.filter(
+              (product) =>
+                  normalizeCategory(product.category) ===
+                  normalizeCategory(selectedCategory)
+          )
+        : products;
+
     return (
         <div className="app">
             <Header />
             <main>
-                <Categories allCategories={allCategories} />
-                <ProductList products={products} />
+                <Categories
+                    allCategories={allCategories}
+                    onCategorySelect={handleCategorySelect}
+                    selectedCategory={selectedCategory}
+                />
+                <ProductList products={filteredProducts} />
             </main>
         </div>
     );
--- week1/project/ecommerce/src/components/Categories.jsx
+++ week1/project/ecommerce/src/components/Categories.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable react/prop-types */
-export default function Categories({ allCategories }) {
+export default function Categories({
+    allCategories,
+    onCategorySelect,
+    selectedCategory
+}) {
     return (
         <section>
             <ul className="categories">
                 {allCategories.map((category, index) => (
-                    <li key={index} className="categories--item">
+                    <li
+                        key={index}
+                        className={`categories--item ${
+                            selectedCategory === category
+                                ? 'categories--item-selected'
+                                : ''
+                        }`}
+                        onClick={() => onCategorySelect(category)}
+                    >
                         {category}
                     </li>
                 ))}
