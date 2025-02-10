e2f34f20295fb004e94614399b16955998209856
Vlada B <vlada.bessikalo@gmail.com>
2025-02-10 13:47:25 +0100

    chore: move components/ProductDetails.jsx to pages/ProductDetails.jsx

--- week2/project/ecommerce/src/App.jsx
+++ week2/project/ecommerce/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import ProductDetails from './components/ProductDetails.jsx';
+import ProductDetails from './pages/ProductDetails.jsx';
 
 export default function App() {
     return (
